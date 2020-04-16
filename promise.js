4075a51fb0a437673f11a2492e699499df71640a
haoyue.wang <843655240@qq.com>
2020-04-16 20:14:04 +0800

    es6

--- promise.js
+++ promise.js
@@ -162,6 +162,17 @@ function resolvePromise(promise2,x,resolve,reject){
       }
       return promise2
     }
+    console.log(1)
+    setTimeout(function(){
+     console.log(2)
+    },0)
+    new promise(function(resolve) {
+      console.log(3)
+      resolve()
+    }).then( () => {
+      console.log(4)
+    })
+    console.log(5)
     //catch方法
     catch(onRejected){
       return this.then(null,onRejected)
